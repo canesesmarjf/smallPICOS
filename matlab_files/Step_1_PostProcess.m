f9b24fbc64f8e9223b33820a9e4e9f2ecc0fe7ed
Juan Francisco Caneses Marin <jfcocaneses@gmail.com>
2023-01-26 23:46:53 +1000

    Completed the IC setup for ion distirbution function and particle weigth profile

--- matlab_files/Step_1_PostProcess.m
+++ matlab_files/Step_1_PostProcess.m
@@ -41,15 +41,26 @@ for i = 1:numel(info.Groups)
     x_p{i} = data{i}{3};
 end 
 
+% Calculate histograms for each species:
+% =========================================================================
+x_m = d.fields.x_m;
+dx = x_m(2) - x_m(1);
+x_edges = x_m(1:end-1) + dx/2;
+[counts_0,~] = histcounts(d.ions_0.x_p,x_edges,"Normalization","count");
+[counts_1,~] = histcounts(d.ions_1.x_p,x_edges,"Normalization","count");
 
+% Plot data:
+% =========================================================================
 figure('color','w')
 subplot(2,1,1)
 box on
 hold on
-x_m = d.fields.x_m;
-dx = x_m(2) - x_m(1);
 plot(d.fields.x_m,d.ions_0.ncp_m*dx,'k.-')
 plot(d.fields.x_m,d.ions_1.ncp_m*dx,'r.-')
+
+plot(x_m(2:end-1),counts_0,'k','LineWidth',2)
+plot(x_m(2:end-1),counts_1,'r','LineWidth',2)
+
 ylim([0,1.2]*max(d.ions_0.ncp_m*dx))
 
 subplot(2,1,2)
@@ -62,30 +73,10 @@ ylim([0,1.2]*max(d.ions_0.n_m))
 figure;
 plot(d.ions_0.x_p,d.ions_0.v_p(:,1),'k.')
 
-figure;
-plot3(d.ions_0.x_p,d.ions_0.v_p(:,1),d.ions_0.v_p(:,2),'k.')
+if 0
+    figure;
+    plot3(d.ions_0.x_p,d.ions_0.v_p(:,1),d.ions_0.v_p(:,2),'k.')
+end
 
 figure;
 plot(d.fields.x_m,d.fields.Bx_m,'k.-')
-
-return
-
-% Plot data:
-% =========================================================================
-
-figure('color','w')
-hold on
-for ii = 1:numel(groupNames)
-    if ~strcmpi(groupNames{ii},'x_p')
-        continue
-    end
-    hx(ii) = plot(x_p{ii});
-    set(hx(ii),'lineStyle','none','marker','.');
-end
-
-figure('color','w')
-hold on
-for ii = 1:numel(groupNames)
-    hv(ii) = plot(x_p{ii},v_p{ii}(:,1));
-    set(hv(ii),'lineStyle','none','marker','.');
-end
